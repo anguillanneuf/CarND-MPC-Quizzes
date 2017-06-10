3caefdf9f0f8a9c1fe5dfb81c8db80398d39083b
anguillanneuf <tianzih@outlook.com>
2017-06-10 17:10:18 -0400

    quiz 1&2 completed

--- global_kinematic_model/src/main.cpp
+++ global_kinematic_model/src/main.cpp
@@ -7,7 +7,9 @@
 // Helper functions
 //
 constexpr double pi() { return M_PI; }
+
 double deg2rad(double x) { return x * pi() / 180; }
+
 double rad2deg(double x) { return x * 180 / pi(); }
 
 const double Lf = 2;
@@ -19,21 +21,27 @@ const double Lf = 2;
 // NOTE: actuators is [delta, a]
 Eigen::VectorXd globalKinematic(Eigen::VectorXd state,
                                 Eigen::VectorXd actuators, double dt) {
-  Eigen::VectorXd next_state(state.size());
-  return next_state;
+    Eigen::VectorXd next_state(state.size());
+
+    next_state[0] = state[0] + state[3] * cos(state[2]) * dt;
+    next_state[1] = state[1] + state[3] * sin(state[2]) * dt;
+    next_state[2] = state[2] + state[3]/Lf * actuators[0] * dt;
+    next_state[3] = state[3] + actuators[1] * dt;
+
+    return next_state;
 }
 
 int main() {
-  // [x, y, psi, v]
-  Eigen::VectorXd state(4);
-  // [delta, v]
-  Eigen::VectorXd actuators(2);
+    // [x, y, psi, v]
+    Eigen::VectorXd state(4);
+    // [delta, v]
+    Eigen::VectorXd actuators(2);
 
-  state << 0, 0, deg2rad(45), 1;
-  actuators << deg2rad(5), 1;
+    state << 0, 0, deg2rad(45), 1;
+    actuators << deg2rad(5), 1;
 
-  // should be [0.212132, 0.212132, 0.798488, 1.3]
-  auto next_state = globalKinematic(state, actuators, 0.3);
+    // should be [0.212132, 0.212132, 0.798488, 1.3]
+    auto next_state = globalKinematic(state, actuators, 0.3);
 
-  std::cout << next_state << std::endl;
+    std::cout << next_state << std::endl;
 }
